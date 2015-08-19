32107720016427b51b592c987db5db47a481c761
Jason.Brewer <jsnbrwr@gmail.com>
2015-08-19 17:00:56 -0500

    Confirm server accessible with first listen.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // Survey app: Node, MySQL
+
+var app = require('express'),
+    Sequelize = require('sequelize');
+
+/*
+// Environment
+var SVY_SQLPORT = 2835;
+
+var sequelize = new Sequelize('mysql://localhost:' + SVY_SQLPORT + '/survey');
+
+// model definition
+var User = sequelize.define("User", {
+    username: Sequelize.STRING,
+    password: Sequelize.STRING
+});
+*/
+
+app.get("/";, function(req, res) {
+    res.send({name:"Hello Wolrd"});
+});
+ 
+// initializing a port
+app.listen(4227);
