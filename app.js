b2bffd63dc0ed89a579d94b61424271adf785ef5
Jason.Brewer <jsnbrwr@gmail.com>
2015-08-19 17:06:42 -0500

    Connect to database.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Survey app: Node, MySQL
 
-var app = require('express'),
+var app = require('express')(),
     Sequelize = require('sequelize');
 
-/*
 // Environment
 var SVY_SQLPORT = 2835;
 
 var sequelize = new Sequelize('mysql://localhost:' + SVY_SQLPORT + '/survey');
 
+/*
 // model definition
 var User = sequelize.define("User", {
     username: Sequelize.STRING,
@@ -16,8 +16,20 @@ var User = sequelize.define("User", {
 });
 */
 
-app.get("/";, function(req, res) {
-    res.send({name:"Hello Wolrd"});
+// check database connection
+sequelize.authenticate().complete(function(err) {
+    if (err)
+    {
+      console.log('Unable to connect to the database:', err);
+    }
+    else
+    {
+      console.log('Connection has been established successfully.');
+    }
+});
+
+app.get("/", function(req, res) {
+    res.send({name:"Hello World"});
 });
  
 // initializing a port
