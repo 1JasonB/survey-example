af78130a9a29f2c6d05ec7876eb7b9f15ec373ed
Jason.Brewer <jsnbrwr@gmail.com>
2015-08-21 12:55:43 -0500

    Client integration with express.

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
 // Survey app: Node, MySQL
 
 var app = require('express')();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var db = require('./models/index')();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', routes);
+// app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 
 // Environment
 var SVY_SQLPORT = 2835,
@@ -43,14 +94,10 @@ app.get('/users', function(req, res) {
     res.status(200).send(SVY_User);
 });
 
-function initUsers()
+function addUser(name, password)
 {
-    //sync the model with the database
-    db.sequelize.sync({ force: true }).then(function() {
-    
-        console.log("DB Synch'd...");
         var User = db.User;
-        var user = User.addUser("guest", "guest", function(err, newUser) {
+        var user = User.addUser(name, password, function(err, newUser) {
             if (newUser)
             {
                 console.log(newUser);
@@ -61,6 +108,14 @@ function initUsers()
                 console.log('ERROR: ' + err);
             }
         });
+}
+
+function initDB()
+{
+    //sync the model with the database
+    db.sequelize.sync({ force: true }).then(function() {
+    
+        console.log("DB Synch'd...");
     }).catch(function(err) {
         console.log('ERROR: ' + err);
     });
@@ -69,4 +124,7 @@ function initUsers()
 // initializing a port
 app.listen(APP_PORT);
 console.log('...listening on port ' + APP_PORT);
-initUsers();
+initDB(function() {
+    addUser('jason','jason');
+});
+
