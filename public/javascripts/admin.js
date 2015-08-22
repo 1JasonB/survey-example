89a937feee8ecba38d28ee7a8aa4c936e35c4aec
Jason.Brewer <jsnbrwr@gmail.com>
2015-08-22 10:15:18 -0500

    Typo?

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,12 +1,15 @@
 angular.module('surveyBuilderAdmin.adminController', [
 ])
-.controller('adminController', function($scope) {
+.controller('AdminController', function($scope) {
     
     console.log('Load adminController');
     $scope.statusMessage = 'Log in to admin console...';
-    /*
     $scope.loginAdmin = function() {
         
+        console.log('Log in admin...');
+    };
+    /*
+     
         $http.post('/login', {username: $scope.username, password: $scope.password})
         .success(function(data, status, headers, config) {
             if (status == 200)
