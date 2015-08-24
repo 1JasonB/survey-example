e9b3c49b2bb61d0ec73caea242d4e596d907caa9
Jason.Brewer <jsnbrwr@gmail.com>
2015-08-24 12:16:53 -0500

    Debugging...

--- public/javascripts/surveyControllers.js
+++ public/javascripts/surveyControllers.js
@@ -1,8 +1,10 @@
 angular.module('surveyBuilder.surveyControllers', [])
 .factory('question', ['$http', function($http){
     var o = {
-        text: 'Sample question',
-        choices: [{text: 'Answer 1'}, {text: 'Answer 2'}, {text: 'Answer 3'}, {text: 'Answer 4'}],
+        question: {
+            text: 'Sample question',
+            choices: [{text: 'Answer 1'}, {text: 'Answer 2'}, {text: 'Answer 3'}, {text: 'Answer 4'}],
+        }
     };
     console.log('Init question factory...');
     o.getQuestion = function() {
@@ -16,9 +18,11 @@ angular.module('surveyBuilder.surveyControllers', [])
             {
                 // No more questions for this user
                 console.log('No more questions.');
-                o.text = 'No more questions';
-                o.choices = [];
+                o.question.text = 'No more questions';
+                o.question.choices = [];
             }
+        }).error(function(error) {
+            console.log('No next question found.');
         });
     };
     return o;
@@ -27,8 +31,8 @@ angular.module('surveyBuilder.surveyControllers', [])
 '$scope',
 'question',
 function($scope, question) {
-    $scope.question = question.text;
-    $scope.choices = question.choices;
+    $scope.question = question.question.text;
+    $scope.choices = question.question.choices;
 
     $scope.submitAnswer = function() {
         console.log('Answered question.');
